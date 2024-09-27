f4e6bb09de9a0a73daf66e72388d5030a88d5b2f
Ravindra <ravindra1s.siddavatam@gmail.com>
2024-09-27 14:03:56 -0500

    first commit

---
+++ src/PassbyvalueTapAcademy.java
@@ -0,0 +1,19 @@
+
+public class PassbyvalueTapAcademy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a =1000;
+		System.out.println(a);
+		
+		int b;
+		b = a;
+		System.out.println(b);
+		b=2000;
+		System.out.println(b);
+		System.out.println(a);
+
+	}
+
+}
